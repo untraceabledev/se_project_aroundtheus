8a9a1653f2b84dcd590dd702bc4a2952ff9f1082
Ziya <zeyahx@gmail.com>
2024-04-18 00:55:49 -0400

    modal

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,15 @@ const initialCards = [
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     }    
   ];
+
+  const profileEditButton = document.querySelector("#profile-edit-button");
+  const profileEditModal = document.querySelector("#profile-edit-modal");
+  const profileEditCloseButton = profileEditModal.quertSelector(".modal__close");
+
+  profileEditButton.addEventListener("click"), () => {
+    profileEditModal.classList.add('modal_opened');
+  }
+
+  profileEditButton.addEventListener("click"), () => {
+    profileEditModal.classList.remove('modal_opened');
+  }
